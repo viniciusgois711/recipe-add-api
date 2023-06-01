84188ce051709f3a338eb25fbbaf4b71feb71de5
Lucas Vinicius <lu.viniteixeira78@gmail.com>
2023-06-01 09:14:14 -0300

    new commit

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,13 +5,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
     
     def test_subtract_number(self):
@@ -19,3 +19,4 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
+        
