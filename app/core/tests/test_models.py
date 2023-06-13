a6198b45b582f9a6ab9302b2386e0847d943be35
Lucas Vinicius <lu.viniteixeira78@gmail.com>
2023-06-13 10:29:09 -0300

    Criando modelo de usuario

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -36,6 +36,7 @@ class ModelTests(TestCase):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+
     def test_create_superuser(self):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
